465250c780ee346d7c380b0489dfb04da633ac84
Ridha Khemiri <khemiri.ridha2013@gmail.com>
2017-03-24 12:39:02 +0100

    site

---
+++ test1/application/config/twitter.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * twconnect library configuration
+ */
+
+$config = array(
+  'consumer_key'    => 'euRz0vnZEgUj5L7faWo20pDfi',
+  'consumer_secret' => 'K7obHeWxT7FVxEUDqictCApkd6f5ygsCg1ltOtIHARfM7uqZN0',
+  'oauth_callback'  => '/welcome/callback' // Default callback application path
+);
+
+?>
